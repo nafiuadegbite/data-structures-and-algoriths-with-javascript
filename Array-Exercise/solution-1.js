1f89bb752ea744e09a99e22d013595368bf7657d
nafiu adegbite <youngguru23@gmail.com>
2022-07-22 22:14:23 +0100

    refractor solution one

--- Array-Exercise/solution-1.js
+++ Array-Exercise/solution-1.js
@@ -1,23 +1,45 @@
+// Question 1. Create a grades object that stores a set of student grades in an object.
+// Provide a function for adding a grade and a function for displaying the student’s grade average.
+
+// Grades Object
 class Grades {
-  constructor() {
+  constructor(name) {
+    this.name = name;
     this.grades = [];
   }
 
+// function for adding grade
   add(grade) {
     this.grades.push(grade);
   }
 
+// function for displaying the student's grade average
   average() {
     let total = this.grades.reduce((a, b) => a + b);
+    let averageGrade = total / this.grades.length;
 
-    return total / this.grades.length;
+    return `${this.name} average grade is ${averageGrade}`;
   }
 }
 
-const studentGrades = new Grades();
+// Grades declaration for the first student
+const studentOne = new Grades("John");
 
-studentGrades.add(25);
-studentGrades.add(55);
-studentGrades.add(96);
+// Adding gardes for the first student
+studentOne.add(25);
+studentOne.add(55);
+studentOne.add(96);
 
-console.log(studentGrades.average());
+// Grades declaration for the second student
+const studentTwo = new Grades("James");
+
+// Adding gardes for the second student
+studentTwo.add(94);
+studentTwo.add(95);
+studentTwo.add(96);
+
+// Display first student average grades
+console.log(studentOne.average());
+
+// Displaying second student average grades
+console.log(studentTwo.average());
